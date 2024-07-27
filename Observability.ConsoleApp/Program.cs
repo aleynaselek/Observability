a81d1f0989273e30159cd56926e4a602f84ef8f6
aleynaflo <selekaleyna@gmail.com>
2024-07-28 00:57:18 +0300

    traceProvider

--- Observability.ConsoleApp/Program.cs
+++ Observability.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Observability.ConsoleApp;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+
 Console.WriteLine("Hello, World!");
+
+var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .ConfigureResource(configure =>
+    {
+        configure.AddService(OpenTelemetryConstants.ServiceName, OpenTelemetryConstants.ServiceVersion)
+                 .AddAttributes(new List<KeyValuePair<string, object>>()
+                 {
+                     new KeyValuePair<string,object>("host.machineName",Environment.MachineName),
+                     new KeyValuePair<string,object>("host.environment", "dev"),
+                 });
+    }).Build();
