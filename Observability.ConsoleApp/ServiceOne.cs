1d8b2db9e776577491fa9155916ff4c2e9489002
aleynaflo <selekaleyna@gmail.com>
2024-07-28 21:04:41 +0300

    7- Jeager ve Zipkin nedir?

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Observability.ConsoleApp;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,13 +16,15 @@ namespace Observability.ConsoleApp
             using var activity = ActivitySourceProvider.Source.StartActivity();
 
             var eventTags = new ActivityTagsCollection();
-            eventTags.Add("userId", 30);
 
-            activity.AddEvent(new("google'a istek başladı", tags: eventTags));
-
+            activity?.AddEvent(new("google'a istek başladı", tags: eventTags));
+             
             var result =await httpClient.GetAsync("https://www.google.com");
+
             var responseContent = await result.Content.ReadAsStringAsync();
 
+            eventTags.Add("google body length", responseContent.Length);
+            activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
             return responseContent.Length;
         }
     }
