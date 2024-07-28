df2cce9b7388490c6763145289d75c5349539748
aleynaflo <selekaleyna@gmail.com>
2024-07-28 21:11:03 +0300

    8-Tracing Nedir?

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -13,19 +13,30 @@ namespace Observability.ConsoleApp
         static HttpClient httpClient = new HttpClient();
         internal async Task<int> MakeRequestToGoogle()
         {
-            using var activity = ActivitySourceProvider.Source.StartActivity();
+            using var activity = ActivitySourceProvider.Source.StartActivity(kind:System.Diagnostics.ActivityKind.Producer, name:"CustomMakeRequestToGoogle");
 
-            var eventTags = new ActivityTagsCollection();
 
-            activity?.AddEvent(new("google'a istek başladı", tags: eventTags));
-             
-            var result =await httpClient.GetAsync("https://www.google.com");
+            try
+            {
+                var eventTags = new ActivityTagsCollection();
 
-            var responseContent = await result.Content.ReadAsStringAsync();
+                activity?.AddEvent(new("google'a istek başladı", tags: eventTags));
 
-            eventTags.Add("google body length", responseContent.Length);
-            activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
-            return responseContent.Length;
+                var result = await httpClient.GetAsync("https://www.google.com");
+
+                var responseContent = await result.Content.ReadAsStringAsync();
+
+                eventTags.Add("google body length", responseContent.Length);
+                activity?.AddEvent(new("google'a istek tamamlandı", tags: eventTags));
+                return responseContent.Length;
+            }
+            catch (Exception ex)
+            {
+                activity.SetStatus(ActivityStatusCode.Error, ex.Message);
+                return 0;
+            }
+
+            
         }
     }
 }
