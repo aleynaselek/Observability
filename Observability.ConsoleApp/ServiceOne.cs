bf7a9cd791919b56c822fa15e151aec5b5bbd736
aleynaflo <selekaleyna@gmail.com>
2024-07-28 20:58:04 +0300

    6-Activity (Span) Kind nedir?

--- Observability.ConsoleApp/ServiceOne.cs
+++ Observability.ConsoleApp/ServiceOne.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,11 @@ namespace Observability.ConsoleApp
         internal async Task<int> MakeRequestToGoogle()
         {
             using var activity = ActivitySourceProvider.Source.StartActivity();
+
+            var eventTags = new ActivityTagsCollection();
+            eventTags.Add("userId", 30);
+
+            activity.AddEvent(new("google'a istek başladı", tags: eventTags));
 
             var result =await httpClient.GetAsync("https://www.google.com");
             var responseContent = await result.Content.ReadAsStringAsync();
